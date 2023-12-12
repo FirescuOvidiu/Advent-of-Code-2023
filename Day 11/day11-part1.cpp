04fd656bc65c325d8b39dccc093bedda271e1e9f
Firescu Ovidiu <ovidiufirescu@gmail.com>
2023-12-12 13:36:17 +0200

    Fix Day 11 part 1 initial solution (added wrong solution from another day)

--- Day 11/day11-part1.cpp
+++ Day 11/day11-part1.cpp
@@ -10,198 +10,165 @@ int main()
   fstream in("input.in", fstream::in);
 
   string    s, l;
-  long long sum = 0, num = 0;
+  long long sum = 0, num = 0, nx = 0, ny = 0;
   char      c{};
 
-  vector<vector<char>> m(10000, vector<char>(10000, '0'));
+  vector<vector<char>> m3(10000, vector<char>(10000, '0'));
 
   long long         max = 0, min = numeric_limits<long long>::max();
   vector<long long> v;
-  int               j = 0;
+
   while (getline(in, l))
   {
     stringstream ss{ l };
 
-    for (size_t i = 0; i < l.size(); i++)
+    for (ny = 0; ny < l.size(); ny++)
     {
-      m[j][i] = l[i];
+      m3[nx][ny] = l[ny];
     }
-    j++;
+    nx++;
   }
 
-  int x, y;
-  for (size_t i = 0; i < m[0].size(); i++)
+  vector<vector<char>> m(nx, vector<char>(ny, '0'));
+
+  vector<vector<char>> m10(nx, vector<char>(ny, '0'));
+
+  for (size_t i = 0; i < nx; i++)
   {
-    for (size_t j = 0; j < m.size(); j++)
-    {
-      if (m[i][j] == 'S')
-      {
-        x = i;
-        y = j;
-      }
-    }
+    for (size_t j = 0; j < ny; j++)
+      m[i][j] = m3[i][j];
   }
 
-  m[x][y] = '|';
+  m10 = m;
 
-  vector<vector<bool>>  visit(m.size(), vector<bool>(m[0].size(), false));
-  vector<vector<int>>   visit2(m.size(), vector<int>(m[0].size(), 0));
-  queue<pair<int, int>> x1;
+  vector<int> rows, columns;
 
-  x1.push({ x, y });
-  visit[x][y]       = true;
-  long long currSum = 0;
-
-  while (!x1.empty())
+  vector<vector<char>> m2;
+  int                  newNx = nx;
+  for (size_t i = 0; i < nx; i++)
   {
-    auto currX = x1.front();
-    x1.pop();
-
-    if (m[currX.first][currX.second] == '|')
+    bool check = true;
+    for (size_t j = 0; j < ny; j++)
     {
-      int xa = currX.first - 1;
-      int xb = currX.second;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
+      if (m[i][j] == '#')
       {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-
-      xa = currX.first + 1;
-      xb = currX.second;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
+        check = false;
       }
     }
-    if (m[currX.first][currX.second] == '-')
+
+    if (check)
     {
-      int xa = currX.first;
-      int xb = currX.second - 1;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-
-      xa = currX.first;
-      xb = currX.second + 1;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
+      m2.push_back(m[i]);
+      newNx++;
     }
-    if (m[currX.first][currX.second] == 'L')
-    {
-      int xa = currX.first - 1;
-      int xb = currX.second;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-
-      xa = currX.first;
-      xb = currX.second + 1;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-    }
-    if (m[currX.first][currX.second] == 'J')
-    {
-      int xa = currX.first - 1;
-      int xb = currX.second;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-
-      xa = currX.first;
-      xb = currX.second - 1;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-    }
-    if (m[currX.first][currX.second] == '7')
-    {
-      int xa = currX.first + 1;
-      int xb = currX.second;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-
-      xa = currX.first;
-      xb = currX.second - 1;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-    }
-    if (m[currX.first][currX.second] == 'F')
-    {
-      int xa = currX.first + 1;
-      int xb = currX.second;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-
-      xa = currX.first;
-      xb = currX.second + 1;
-
-      if (checkInMap(xa, xb, m.size(), m[0].size()) && visit[xa][xb] == false && m[xa][xb] != '.')
-      {
-        x1.push({ xa, xb });
-        visit[xa][xb]  = true;
-        visit2[xa][xb] = visit2[currX.first][currX.second] + 1;
-      }
-    }
+    m2.push_back(m[i]);
   }
 
-  int max2 = 0;
-  for (int i = 0; i < m[0].size(); i++)
+  m = m2;
+
+  nx = newNx;
+
+  set<int> c5;
+
+  for (size_t i = 0; i < ny; i++)
   {
-    for (int j = 0; j < m.size(); j++)
+    bool check = true;
+    for (size_t j = 0; j < nx; j++)
     {
-      if (visit2[i][j] > max2)
-        max2 = visit2[i][j];
+      if (m[j][i] == '#')
+      {
+        check = false;
+      }
+    }
+
+    if (check)
+      c5.insert(i);
+  }
+
+  m2.clear();
+
+  int newNy = ny;
+  for (size_t i = 0; i < nx; i++)
+  {
+    bool         check = true;
+    vector<char> newV;
+    for (size_t j = 0; j < ny; j++)
+    {
+      if (c5.find(j) != c5.end())
+      {
+        if (i == 0)
+          newNy++;
+        newV.push_back(m[i][j]);
+      }
+      newV.push_back(m[i][j]);
+    }
+    m2.push_back(newV);
+  }
+
+  ny = newNy;
+  m  = m2;
+
+  vector<pair<int, int>> g;
+
+  for (size_t i = 0; i < nx; i++)
+  {
+    for (size_t j = 0; j < ny; j++)
+    {
+      if (m[i][j] == '#')
+        g.push_back({ i, j });
     }
   }
 
-  cout << max2;
+  int totalsum = 0;
 
-  cout << endl;
+  for (size_t i = 0; i < g.size(); i++)
+  {
+    for (size_t j = i + 1; j < g.size(); j++)
+    {
+      vector<vector<bool>>  visit(nx, vector<bool>(ny, false));
+      queue<pair<int, int>> x1;
+      queue<int>            sums;
+
+      vector<int> dx{ 0, 1, 0, -1, -1, -1, +1, +1 };
+      vector<int> dy{ -1, 0, 1, 0, -1, +1, -1, +1 };
+
+      auto first  = g[i];
+      auto second = g[j];
+
+      x1.push({ first.first, first.second });
+      sums.push({ 0 });
+
+      visit[first.first][first.second] = true;
+      long long currSum                = 0;
+
+      while (!x1.empty())
+      {
+        auto currX = x1.front();
+        auto ssss  = sums.front();
+        sums.pop();
+        x1.pop();
+
+        if ((currX.first == second.first) && (currX.second == second.second))
+        {
+          totalsum += ssss;
+          break;
+        }
+
+        for (int iz = 0; iz < 4; iz++)
+        {
+          int xa = currX.first + dx[iz];
+          int xb = currX.second + dy[iz];
+
+          if (checkInMap(xa, xb, nx, ny) && visit[xa][xb] == false)
+          {
+            x1.push({ xa, xb });
+            sums.push({ ssss + 1 });
+            visit[xa][xb] = true;
+          }
+        }
+      }
+    }
+  }
+
+  cout << endl << totalsum;
 }
